48294ec9ce0049a1e96ccd0c9127923a2d13192c
githubofcjs <1042125947@qq.com>
2019-05-02 20:09:48 +0800

    commit3

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,28 +1,28 @@
 #R Version 3.3.3
 
 makeCacheMatrix<- function(x = matrix()) {
-  #create a variable 's' and set its value as NULL
-  s <- NULL
+  #create a variable 'sv' and set its value as NULL
+  sv <- NULL
   
   #Creat a function named 'set' which would set 'x' as same as 'y' 
-  #and set 's' as NULL ('x' and 's' are values to 
+  #and set 'sv' as NULL ('x' and 'sv' are values to 
   #objects in an environment that is different from the
   #current environment)
   set <- function(y) {
     x <<- y
-    s <<- NULL
+    sv <<- NULL
   }
   
   #Create a function 'get' which would return 'x' without any input argments
   get <- function() x
   
-  #Create a function 'setsolve' that would set 's' 
+  #Create a function 'setsolve' that would set 'sv' 
   #as the bult-in function 'solve'
-  setsolve <- function(solve) s <<- solve
+  setsolve <- function(solve) sv <<- solve
   
   #Create a function 'getsolve' 
-  #which would return 's' without any input argments
-  getsolve <- function() s
+  #which would return 'sv' without any input argments
+  getsolve <- function() sv
   
   #return a list contain 'set','get','setsolve' and 'getsolve'
   list(set = set, get = get,
