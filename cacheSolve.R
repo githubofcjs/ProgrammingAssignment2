04a6263333bf813d04d853efbc07544f61f2a50a
githubofcjs <1042125947@qq.com>
2019-05-02 19:30:56 +0800

    commit

--- cacheSolve.R
+++ cacheSolve.R
@@ -1,11 +1,47 @@
+#Assuming that 'x' has been set by makeCacheMatrix with
+# a square matrix as its input argment
+
 cacheSolve <- function(x, ...) {
-  m <- x$getsolve()
-  if(!is.null(m)) {
+  
+  #Set a variable 's' as the build-in function 'solve'
+  s <- x$getsolve()
+  
+    #Note: 's' has got cached data if it's not NULL(it's)
+  if(!is.null(s)) {
+    #Send "getting cached data" to the console
     message("getting cached data")
-    return(m)
+    #As we know, 's' hasn't inited correctly, so we return 's',
+    #and the whole function end
+    return(s)
   }
+  
+  #The 's' is NULL, which means 'x' is a correct input argrument,
+  #the implemention continue
+  
+  #Creat a new variable 'data' and send the argument we had sent to 
+  #function 'makeCacheMatrix' before to 'data', 
+  #which is a preparation for 
+  #calculating the the argument's inverse matrix
   data <- x$get()
-  m <- solve(data, ...)
-  x$setsolve(m)
-  m
+  
+  #Calculate the the argument's inverse matrix and send it to 's'
+  s <- solve(data, ...)
+  
+  #Send the matrix we got from last step to the 
+  #varible 's'in function 'makeCacheVector'
+  # The value of 'x$getsolve()' would be returned directly if we implement the 
+  #function with the same argument second time
+  
+  #example:
+  #a<-makeCacheMatrix(x)
+  #cacheSolve(a)
+  #output:[1] inv_matrix(a)  ---the inverse matrix of 'a'
+  #cacheSolve(a)
+  #output:
+  #getting cached data  -------message we set in if structure
+  #[1] inv_matrix(a)        ---the inverse matrix of 'a'
+  x$setsolve(s)
+  
+  #Return 's'
+  s
 }
--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,12 +1,28 @@
 makeCacheMatrix<- function(x = numeric()) {
-  m <- NULL
+  #create a variable 's' and set its value as NULL
+  s <- NULL
+  
+  #Creat a function named 'set' which would set 'x' as same as 'y' 
+  #and set 's' as NULL ('x' and 's' are values to 
+  #objects in an environment that is different from the
+  #current environment)
   set <- function(y) {
     x <<- y
-    m <<- NULL
+    s <<- NULL
   }
+  
+  #Create a function 'get' which would return 'x' without any input argments
   get <- function() x
-  setsolve <- function(solve) m <<- solve
-  getsolve <- function() m
+  
+  #Create a function 'setsolve' that would set 's' 
+  #as the bult-in function 'solve'
+  setsolve <- function(solve) s <<- solve
+  
+  #Create a function 'getsolve' 
+  #which would return 's' without any input argments
+  getsolve <- function() s
+  
+  #return a list contain 'set','get','setsolve' and 'getsolve'
   list(set = set, get = get,
        setsolve = setsolve,
        getsolve = getsolve)
